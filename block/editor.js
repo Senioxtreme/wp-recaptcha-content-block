acf6afe002b33046389f6b08ce1ff830123419bf
Michele Gentilini <51083221+mrgenty@users.noreply.github.com>
2024-09-15 17:23:25 +0200

    Enhanced block editor to support button text customization and inner blocks
    - Added the ability to customize the button text directly within the Gutenberg block editor.
    - Integrated InnerBlocks to allow the inclusion of multiple types of content inside the reCAPTCHA protected area.
    - Improved user interface with InspectorControls for easy customization of button text.

--- block/editor.js
+++ block/editor.js
@@ -1,43 +1,66 @@
 ( function( blocks, element, blockEditor ) {
     var el = element.createElement;
     var RichText = blockEditor.RichText;
-
-    console.log('reCAPTCHA Content Protection caricato correttamente.');
+    var InnerBlocks = blockEditor.InnerBlocks;
+    var InspectorControls = blockEditor.InspectorControls;
+    var TextControl = wp.components.TextControl;
 
     blocks.registerBlockType( 'rcb/recaptcha-content-block', {
-        title: 'reCAPTCHA Content Protection',
+        title: 'Contenuto Protetto da reCAPTCHA',
         icon: 'shield',
         category: 'common',
         attributes: {
             content: {
                 type: 'string',
             },
+            buttonText: {
+                type: 'string',
+                default: 'Mostra il contenuto'
+            },
         },
         edit: function( props ) {
             var content = props.attributes.content;
+            var buttonText = props.attributes.buttonText;
             var setAttributes = props.setAttributes;
             var className = props.className;
 
-            function onChangeContent( newContent ) {
-                setAttributes( { content: newContent } );
+            function onChangeButtonText( newText ) {
+                setAttributes( { buttonText: newText } );
             }
 
             return el(
                 'div',
                 { className: className },
                 el(
-                    RichText,
-                    {
-                        tagName: 'div',
-                        placeholder: 'Inserisci il contenuto protetto...',
-                        value: content,
-                        onChange: onChangeContent,
-                    }
+                    InspectorControls,
+                    {},
+                    el(
+                        TextControl,
+                        {
+                            label: 'Testo del Pulsante',
+                            value: buttonText,
+                            onChange: onChangeButtonText,
+                        }
+                    )
+                ),
+                el(
+                    'div',
+                    { className: 'rcb-button' },
+                    el( RichText, {
+                        tagName: 'button',
+                        value: buttonText,
+                        onChange: onChangeButtonText,
+                    })
+                ),
+                el(
+                    'div',
+                    { className: 'rcb-protected-content' },
+                    el( InnerBlocks )
                 )
             );
         },
         save: function() {
-            return null;
+            return el( 'div', {}, el( InnerBlocks.Content ) );
         },
     } );
 } )( window.wp.blocks, window.wp.element, window.wp.blockEditor );
