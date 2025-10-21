3af6ace93d92020b4b5cabadf17485d8bf716a72
Michele Gentilini <51083221+mrgenty@users.noreply.github.com>
2025-10-21 19:06:11 +0200

    Update editor.js

--- block/editor.js
+++ block/editor.js
@@ -1,31 +1,44 @@
-( function( blocks, element, blockEditor ) {
+( function( blocks, element, blockEditor, components, i18n ) {
     var el = element.createElement;
+    var __ = i18n.__;
     var RichText = blockEditor.RichText;
     var InnerBlocks = blockEditor.InnerBlocks;
     var InspectorControls = blockEditor.InspectorControls;
-    var TextControl = wp.components.TextControl;
+    var TextControl = components.TextControl;
+    var PanelBody = components.PanelBody;
+    var SelectControl = components.SelectControl;
 
     blocks.registerBlockType( 'rcb/recaptcha-content-block', {
-        title: 'Sezione reCAPTCHA',
-        icon: 'shield',
+        title: __( 'Sezione Protetta CAPTCHA', 'rcb-recaptcha-block' ),
+        icon: 'shield-alt',
         category: 'common',
         attributes: {
-            content: {
-                type: 'string',
-            },
             buttonText: {
                 type: 'string',
-                default: 'Mostra il contenuto'
+                default: __( 'Mostra il contenuto', 'rcb-recaptcha-block' ),
+            },
+            theme: {
+                type: 'string',
+                default: 'light',
+            },
+            mode: {
+                type: 'string',
+                default: 'normal',
             },
         },
         edit: function( props ) {
-            var content = props.attributes.content;
-            var buttonText = props.attributes.buttonText;
+            var { buttonText, theme, mode } = props.attributes;
             var setAttributes = props.setAttributes;
             var className = props.className;
 
             function onChangeButtonText( newText ) {
                 setAttributes( { buttonText: newText } );
+            }
+            function onChangeTheme( newTheme ) {
+                setAttributes( { theme: newTheme } );
+            }
+            function onChangeMode( newMode ) {
+                setAttributes( { mode: newMode } );
             }
 
             return el(
@@ -35,27 +48,61 @@
                     InspectorControls,
                     {},
                     el(
-                        TextControl,
-                        {
-                            label: 'Testo del Pulsante',
-                            value: buttonText,
-                            onChange: onChangeButtonText,
-                        }
+                        PanelBody,
+                        { title: __( 'Impostazioni CAPTCHA', 'rcb-recaptcha-block' ) },
+                        el(
+                            TextControl,
+                            {
+                                label: __( 'Testo del Pulsante', 'rcb-recaptcha-block' ),
+                                value: buttonText,
+                                onChange: onChangeButtonText,
+                            }
+                        ),
+                        el(
+                            SelectControl,
+                            {
+                                label: __( 'Tema (reCAPTCHA/hCaptcha)', 'rcb-recaptcha-block' ),
+                                value: theme,
+                                options: [
+                                    { label: __( 'Chiaro', 'rcb-recaptcha-block' ), value: 'light' },
+                                    { label: __( 'Scuro', 'rcb-recaptcha-block' ), value: 'dark' },
+                                ],
+                                onChange: onChangeTheme,
+                            }
+                        ),
+                        el(
+                            SelectControl,
+                            {
+                                label: __( 'Modalità', 'rcb-recaptcha-block' ),
+                                value: mode,
+                                options: [
+                                    { label: __( 'Normale (Visibile)', 'rcb-recaptcha-block' ), value: 'normal' },
+                                    { label: __( 'Invisibile', 'rcb-recaptcha-block' ), value: 'invisible' },
+                                ],
+                                onChange: onChangeMode,
+                                help: __( 'La modalità invisibile non mostra la sfida a meno che l\'utente non sia sospetto.', 'rcb-recaptcha-block' )
+                            }
+                        )
                     )
                 ),
                 el(
                     'div',
-                    { className: 'rcb-button' },
+                    { className: 'rcb-button-preview' },
                     el( RichText, {
                         tagName: 'button',
                         value: buttonText,
                         onChange: onChangeButtonText,
+                        withoutInteractiveFormatting: true,
+                        allowedFormats: [],
+                        placeholder: __( 'Testo pulsante...', 'rcb-recaptcha-block' ),
                     })
                 ),
                 el(
                     'div',
-                    { className: 'rcb-protected-content' },
-                    el( InnerBlocks )
+                    { className: 'rcb-protected-content-editor' },
+                    el( InnerBlocks, {
+                        renderAppender: InnerBlocks.ButtonBlockAppender,
+                    } )
                 )
             );
         },
@@ -63,4 +110,5 @@
             return el( 'div', {}, el( InnerBlocks.Content ) );
         },
     } );
-} )( window.wp.blocks, window.wp.element, window.wp.blockEditor );
+} )( window.wp.blocks, window.wp.element, window.wp.blockEditor, window.wp.components, window.wp.i18n );
+
